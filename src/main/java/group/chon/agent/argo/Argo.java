b1890d1741bb4594b63dcbed01cfbb4062906512
Nilson Lazarin <nilsonmori@yahoo.com>
2026-02-21 18:11:31 -0300

    ARGO with adjustable timeout (2xlimit)

--- src/main/java/group/chon/agent/argo/Argo.java
+++ src/main/java/group/chon/agent/argo/Argo.java
@@ -6,19 +6,22 @@ import jason.asSyntax.Literal;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Logger;
+
 import jason.architecture.AgArch;
 
 public class Argo extends AgArch {
 
+    private Logger logger;
     public static final String DEFAULT_PORT = "COM1";
 
-    public Javino javino = new Javino();
+    private Javino javino = new Javino();
 
     private String port = "";
 
-    long lastPerceived = 0;
+    private long lastPerceived = 0;
 
-    private long limit = 0;
+    private int limit = 250;
 
     public Boolean blocked = true;
 
@@ -35,18 +38,18 @@ public class Argo extends AgArch {
     @Override
     public void init() throws Exception {
         super.init();
+        this.logger = Logger.getLogger(getAgName());
+        this.javino.infoPortStatus(true);
+        this.javino.timeout(2000);
         this.setPort(DEFAULT_PORT);
     }
 
     @Override
     public Collection<Literal> perceive() {
         long perceiving = System.nanoTime();
+        if (((perceiving - getLastPerceived()) < getLimit()) || isBlocked()) {return null;}
 
-        if (((perceiving - this.lastPerceived) < this.limit) || this.blocked) {
-            return null;
-        }
-        this.lastPerceived = perceiving;
-
+        logger.fine("[ARGO] Perceiving...");
         int cont;
         List<Literal> jPercept = new ArrayList<Literal>();
         try {
@@ -63,17 +66,19 @@ public class Argo extends AgArch {
                     this.getTS().getLogger().warning("[WARNING] There is no message coming from sensors.");
                 }
             }
-
-            return jPercept;
+            this.setLastPerceived();
         } catch (Exception e) {
+            this.setLastPerceived();
             return null;
         }
+        logger.fine("[ARGO] Perceived!");
+        return jPercept;
     }
 
-    @Override
+    /*@Override
     public boolean canSleep() {
         return false;
-    }
+    }*/
 
     public Javino getJavino() {
         return this.javino;
@@ -95,19 +100,20 @@ public class Argo extends AgArch {
         return this.lastPerceived;
     }
 
-    public void setLastPerceived(long lastPerceived) {
-        this.lastPerceived = lastPerceived;
+    public void setLastPerceived() {
+        this.lastPerceived = System.nanoTime();
     }
 
     public long getLimit() {
         return this.limit;
     }
 
-    public void setLimit(long limit) {
+    public void setLimit(int limit) {
+        this.javino.timeout(2*limit);
         this.limit = limit;
     }
 
-    public Boolean getBlocked() {
+    public Boolean isBlocked() {
         return this.blocked;
     }
 
--- src/main/java/group/chon/agent/argo/jasonStdLib/act.java
+++ src/main/java/group/chon/agent/argo/jasonStdLib/act.java
@@ -16,11 +16,11 @@ public class act extends DefaultInternalAction {
 
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
-
          final Argo argoArch = Argo.getArgoArch(ts.getAgArch());
             if (argoArch != null) {
                 Term action = args[0];
                 if (argoArch.getJavino().sendCommand(argoArch.getPort(), action.toString())) {
+                    argoArch.setLastPerceived();
                     return true;
                 } else {
                     String PORT = argoArch.getPort();
@@ -30,6 +30,7 @@ public class act extends DefaultInternalAction {
                     }
                     ts.getAg().getBB().remove(Literal.parseLiteral("port("+PORTshortNAME+",on);"));
                     ts.getAg().getBB().add(Literal.parseLiteral("port("+PORTshortNAME+",off);"));
+                    argoArch.setLastPerceived();
                     return false;
                 }
             }else{
